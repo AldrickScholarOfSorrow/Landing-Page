8b42ce39a1a8c4d23a82b9e279868a828b248e34
AldrickScholarOfSorrow <pedrosfreis@gmail.com>
2025-11-25 19:46:18 -0300

    fix: Ajusta configuração do CORS para permitir todas as origens

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,8 @@ from controllers.conflito_controller import conflito_bp
 # Cria a instância da aplicação Flask
 app = Flask(__name__)
 
-# Lista de origens permitidas.
-origins = [
-    "https://landing-page-sigma-eosin.vercel.app", # Frontend em produção na Vercel
-    "http://localhost:5173"                        # Para desenvolvimento local
-]
-
-# Habilita o CORS, especificando as origens permitidas para todos os recursos.
-CORS(app, resources={r"/*": {"origins": origins}})
+# Habilita o CORS para todas as origens em todas as rotas.
+CORS(app)
 
 
 # Registra o Blueprint que contém as rotas de conflitos
