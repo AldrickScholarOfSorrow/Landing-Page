573f56c249ce873e5fd94b4a49e8bda8055fec6f
AldrickScholarOfSorrow <pedrosfreis@gmail.com>
2025-11-25 17:50:28 -0300

    fix: Configura CORS para ambiente de produção

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,16 @@ from controllers.conflito_controller import conflito_bp
 
 # Cria a instância da aplicação Flask
 app = Flask(__name__)
-# Habilita o CORS para permitir requisições de qualquer origem.
-CORS(app)
+
+# Lista de origens permitidas.
+origins = [
+    "http://localhost:5173",  # Para desenvolvimento local
+    "https://landing-page-sigma-eosin.vercel.app"  # Seu site em produção na Vercel
+]
+
+# Habilita o CORS, especificando as origens permitidas para todos os recursos.
+CORS(app, resources={r"/*": {"origins": origins}})
+
 
 # Registra o Blueprint que contém as rotas de conflitos
 app.register_blueprint(conflito_bp)
@@ -15,4 +23,3 @@ if __name__ == '__main__':
     # O debug=True reinicia o servidor automaticamente a cada alteração no código.
     # O host="0.0.0.0" torna o servidor acessível na sua rede local.
     app.run(host="0.0.0.0", port=5000, debug=True)
-
